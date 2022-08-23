7f39b39dcca3c4b2e2895e3cad463e67f9a91a16
Rami Bukhari <rbisthebest@hotmail.com>
2022-08-23 23:18:49 +0300

    Initial commit

---
+++ unity_project/ECSBattle/Assets/Scenes/Scripts/PlayerMovementSystem.cs
@@ -0,0 +1,15 @@
+using Unity.Entities;
+using Unity.Transforms;
+
+public partial class PlayerMovementSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float delta = Time.DeltaTime;
+        Entities.ForEach((ref Translation translation, in UnitComponentData unitComponent) =>
+        {
+            translation.Value.x += unitComponent.value * delta;
+        }).ScheduleParallel();
+
+    }
+}
