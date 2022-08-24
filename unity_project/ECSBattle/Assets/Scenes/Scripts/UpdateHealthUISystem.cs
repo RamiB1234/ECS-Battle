ec4ac280f423090811d6af990bd4f969c00a2cb2
Rami Bukhari <rbisthebest@hotmail.com>
2022-08-25 00:09:52 +0300

    Show health UI above units

---
+++ unity_project/ECSBattle/Assets/Scenes/Scripts/UpdateHealthUISystem.cs
@@ -0,0 +1,16 @@
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public partial class UpdateHealthUISystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((HealthUIManagedComponent healthUIManagedComponent, in UnitComponentData unitComponentData, in Translation translation) =>
+        {
+            healthUIManagedComponent.healthText.text = unitComponentData.healthPoints.ToString();
+            healthUIManagedComponent.healthText.transform.position =
+            new Vector3(translation.Value.x, translation.Value.y+ healthUIManagedComponent.buffer, translation.Value.z);
+        }).WithoutBurst().Run();
+    }
+}
